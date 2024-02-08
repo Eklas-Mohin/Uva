818c00afb688554c39ce507cfa6fd38ca638a69f
Eklas-Mohin <mohin.030299@gmail.com>
2024-02-08 13:38:42 +0600

    374 - Big Mod (Binary Exponentiation)

--- big_mod.cpp
+++ big_mod.cpp
@@ -6,15 +6,15 @@
 
 using namespace std;
 
-long long binaryExponentiation(long long x, long long y, long long n) {
-    if (y == 0) {
+long long binaryExponentiation(long long b, long long p, long long m) {
+    if (p == 0) {
         return 1;
     }
-    long long res = binaryExponentiation(x, y / 2, n);
-    if (y % 2) {
-        return (res * res * x) % n;
+    long long res = binaryExponentiation(b, p / 2, m);
+    if (p % 2) {
+        return ((res * res * b) % m);
     } else {
-        return (res * res) % n;
+        return ((res * res) % m);
     }
 }
 
@@ -24,7 +24,7 @@ int main() {
     long long b{}, p{}, m{}, ans{};
     while (cin >> b >> p >> m) {
         ans = binaryExponentiation(b, p, m);
-        cout << ans << endl;
+        cout << (ans % m)<< endl;
     }
     return 0;
 }
